136ae7db248ae6de9db6763313bc24eb2360210c
Linerre <leon.errelin@outlook.com>
2021-11-15 20:47:30 +0800

    override emacs to use emacs-28 with native-comp

--- emacs-override.nix
+++ emacs-override.nix
@@ -1,14 +1,16 @@
 with (import <nixpkgs> {});
 
 {
-  emacs-head = (emacs-nox.overrideAttrs (old : {
+  emacs-28 = (emacs-nox.overrideAttrs (old : {
     pname = "emacs";
     version = "head";
     src = fetchFromGitHub {
       owner = "emacs-mirror";
       repo = "emacs";
-      rev = "4d439744685b6b2492685124994120ebd1fa4abb";
-      sha256 = "00vxb83571r39r0dbzkr9agjfmqs929lhq9rwf8akvqghc412apf";
+      rev = "044dd1e21028fad3cf8d976dae887503dbab6ae3";
+      #rev = "4d439744685b6b2492685124994120ebd1fa4abb";
+      #sha256 = "00vxb83571r39r0dbzkr9agjfmqs929lhq9rwf8akvqghc412apf";
+      sha256 = "1s3arbkzwbip8fn56439qf7ic864wyshprx0h0lsn7hh6dndd26q";
     };
     patches = [];
     configureFlags = old.configureFlags ++ ["--with-json"];
@@ -18,14 +20,16 @@ with (import <nixpkgs> {});
     nativeComp = true;
   };
 
-  emacs-head-nox = (emacs-nox.overrideAttrs (old : {
+  emacs-28-nox = (emacs-nox.overrideAttrs (old : {
     pname = "emacs-nox";
     version = "head";
     src = fetchFromGitHub {
       owner = "emacs-mirror";
       repo = "emacs";
-      rev = "4d439744685b6b2492685124994120ebd1fa4abb";
-      sha256 = "00vxb83571r39r0dbzkr9agjfmqs929lhq9rwf8akvqghc412apf";
+      rev = "044dd1e21028fad3cf8d976dae887503dbab6ae3";
+      #rev = "4d439744685b6b2492685124994120ebd1fa4abb";
+      #sha256 = "00vxb83571r39r0dbzkr9agjfmqs929lhq9rwf8akvqghc412apf";
+      sha256 = "1s3arbkzwbip8fn56439qf7ic864wyshprx0h0lsn7hh6dndd26q";
     };
     patches = [];
     configureFlags = old.configureFlags ++ ["--with-json"];

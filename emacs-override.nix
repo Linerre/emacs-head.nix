ccd25b2135e2154ece548a0710dba64b023a5773
Linerre <leon.errelin@outlook.com>
2022-05-09 14:25:24 +0800

    Update emacs drv

--- emacs-override.nix
+++ emacs-override.nix
@@ -1,35 +1,17 @@
 with (import <nixpkgs> {});
 
 {
-  emacs-28 = (emacs-nox.overrideAttrs (old : {
+  emacs-28 = (emacs.overrideAttrs (old : {
     pname = "emacs";
+    # with version remaining head, this is indeed v-29
     version = "head";
     src = fetchFromGitHub {
       owner = "emacs-mirror";
       repo = "emacs";
-      rev = "044dd1e21028fad3cf8d976dae887503dbab6ae3";
+      rev = "d8a47823103da803b8274d790527f48d85a3f9b4";
       #rev = "4d439744685b6b2492685124994120ebd1fa4abb";
       #sha256 = "00vxb83571r39r0dbzkr9agjfmqs929lhq9rwf8akvqghc412apf";
-      sha256 = "1s3arbkzwbip8fn56439qf7ic864wyshprx0h0lsn7hh6dndd26q";
-    };
-    patches = [];
-    configureFlags = old.configureFlags ++ ["--with-json"];
-    preConfigure = "./autogen.sh";
-    buildInputs = old.buildInputs ++ [ autoconf texinfo ];
-  })).override {
-    nativeComp = true;
-  };
-
-  emacs-28-nox = (emacs-nox.overrideAttrs (old : {
-    pname = "emacs-nox";
-    version = "head";
-    src = fetchFromGitHub {
-      owner = "emacs-mirror";
-      repo = "emacs";
-      rev = "044dd1e21028fad3cf8d976dae887503dbab6ae3";
-      #rev = "4d439744685b6b2492685124994120ebd1fa4abb";
-      #sha256 = "00vxb83571r39r0dbzkr9agjfmqs929lhq9rwf8akvqghc412apf";
-      sha256 = "1s3arbkzwbip8fn56439qf7ic864wyshprx0h0lsn7hh6dndd26q";
+      sha256 = "sha256-bVeTVA95/G4MN653EOP0plR7E0KQJil5HKWfDwawVMU=";
     };
     patches = [];
     configureFlags = old.configureFlags ++ ["--with-json"];
